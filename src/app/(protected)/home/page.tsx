548063bebfef586ae67b25737adfa8cd88a1c74b
0xPenryn <ian@world.org>
2025-08-07 20:39:33 -0700

    remove unneeded stuff

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,10 +1,7 @@
 import { auth } from '@/auth';
 import { Page } from '@/components/PageLayout';
-import { Pay } from '@/components/Pay';
 import { Transaction } from '@/components/Transaction';
 import { UserInfo } from '@/components/UserInfo';
-import { Verify } from '@/components/Verify';
-import { ViewPermissions } from '@/components/ViewPermissions';
 import { Marble, TopBar } from '@worldcoin/mini-apps-ui-kit-react';
 
 export default async function Home() {
@@ -27,10 +24,7 @@ export default async function Home() {
       </Page.Header>
       <Page.Main className="flex flex-col items-center justify-start gap-4 mb-16">
         <UserInfo />
-        <Verify />
-        <Pay />
         <Transaction />
-        <ViewPermissions />
       </Page.Main>
     </>
   );
